1b94e332ef764c2e300c4209306c450587bac6af
dotrya <arthur.fen-chong@esme.fr>
2022-05-30 16:38:30 +0200

    header V2

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         
         <?php // Feuille de style ?>
-        <link rel="stylesheet" href="../style.css">
+        <link rel="stylesheet" href="style.css">
 
         <?php // Titre principal et icône de la page ?>
-        <link rel="icon" type="image/png" sizes="16x16" href="../images/deathstarw.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/deathstarw.png">
