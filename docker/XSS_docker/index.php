ee8df0b82fed5e8c2f7ac5380202e94979a7104c
dotrya <arthur.fen-chong@esme.fr>
2022-06-01 11:08:38 +0200

    XSS challenge

---
+++ docker/XSS_docker/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LOGIN</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <form action="log.php" method="POSTS">
+        <h2>LOGIN</h2>
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Username"><br>
+        <label>Password</label>
+        <input type="text" name="password" placeholder="Password"><br>
+        <button type="submit">Login</button>
+    </form>
+</html>
